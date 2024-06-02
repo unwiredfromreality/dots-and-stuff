e7c31e4c7eb409bcbf284b5089512e737b635a08
Riley Padillo <rileypadillo@gmail.com>
2024-06-02 15:22:12 +0800

    add

---
+++ .config/fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+function fish_greeting
+     pokemon-colorscripts -r --no-title
+     fastfetch
+end
+
+fish_add_path /home/riley/.spicetify
