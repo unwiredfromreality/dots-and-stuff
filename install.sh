f8f9d6e29ab5f4ad2af1600a110860cb7b7d49a9
unwiredfromreality <168145034+unwiredfromreality@users.noreply.github.com>
2024-06-06 05:41:06 +0800

    add install script

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+sudo pacman -S --noconfirm --needed wget base-devel git rofi-wayland dunst swayidle swaybg waybar ly kitty caja atril eom engrampa mate-polkit firefox pavucontrol blueman fastfetch networkmanager fish kanshi ttc-iosevka-aile ttf-iosevkatermslab-nerd adobe-source-han-sans-otc-fonts kvantum qt6ct grim slurp swappy xdg-user-dirs pipewire pipewire-pulse pipewire-alsa wireplumber alsa-utils brightnessctl
+git clone https://github.com/unwiredfromreality/dots-and-stuff.git
+cd dots-and-stuff/
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin/
+makepkg -si
+cd ..
+yay -S --noconfirm labwc-git nwg-drawer-bin gtklock pokemon-colorscripts-git resources
+cp -r .config ~/
+cp -r .local ~/
+sudo systemctl enable ly.service
+sudo systemctl enable NetworkManager.service
+sudo systemctl enable bluetooth.service
+systemctl enable --user wireplumber.service
+gsettings set org.gnome.desktop.interface cursor-theme Capitaine-Cursors-Gruvbox-White
+gsettings set org.gnome.desktop.interface cursor-size 40 
+gsettings set org.gnome.desktop.interface font-name 'Iosevka Aile'
+gsettings set org.gnome.desktop.interface monospace-font-name 'IosevkaTermSlab Nerd Font'
+gsettings set org.gnome.desktop.interface gtk-theme Gruvbox-Material-Dark
+gsettings set org.gnome.desktop.interface icon-theme Gruvbox-Plus-Dark
+gsettings set org.gnome.desktop.interface color-scheme prefer-dark
+xdg-user-dirs-update
+xdg-mime default caja.desktop inode/directory
+wget https://i.ibb.co/8B1tkBH/wallhaven-l87rxp.jpg -O ~/Pictures/wallpaper.jpg
+echo "Rebooting system in 5 seconds..."
+echo "After rebooting, you may now login to labwc session."
+sleep 5
+sudo reboot
