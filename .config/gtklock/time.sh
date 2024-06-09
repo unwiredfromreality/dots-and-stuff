1d47f8dbbfc870b76176d675695c8924e6b5143a
Riley Padillo <rileypadillo@gmail.com>
2024-06-10 05:09:40 +0800

    allow changing ls clock color from clock-settings

--- .config/gtklock/time.sh
+++ .config/gtklock/time.sh
@@ -2,6 +2,6 @@
 source ~/.config/gtklock/clock-settings
 
 date=$(date +"%A, %B %-d")
-echo '<b>'"$date"'</b>'
+echo '<span foreground="'$LS_FONT_COLOR'">''<b>'"$date"'</b>''</span>'
 time=$(date +"%-I:%M")
-echo '<span size="'$LS_FONT_SIZE'" face="'$LS_FONT_FACE'" weight="'$LS_FONT_WEIGHT'">'"$time"'</span>'
+echo '<span foreground="'$LS_FONT_COLOR'" size="'$LS_FONT_SIZE'" face="'$LS_FONT_FACE'" weight="'$LS_FONT_WEIGHT'">'"$time"'</span>'
